8ad2c8d2da6db1a0dba7be534834639d51072bd0
skomorohovaleks <skomorohovalexksandr52@gmail.com>
2025-01-03 17:47:51 +0300

    bug fixed, add text menu

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class WebScraper:
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
             }
 
-            async with session.get(url, timeout=20, headers=headers) as response:
+            async with session.get(url, timeout=60, headers=headers) as response:
                 response.raise_for_status()  # Вызывает исключение для кода ответа 4xx/5xx
                 return await response.text()
         except aiohttp.ClientError as e:
@@ -24,27 +24,29 @@ class WebScraper:
             return None
 
     async def get_html_data(self):
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
             tasks = []
 
             for url in self.urls:
                 tasks.append(self.fetch_html(url, session))
 
-            return await asyncio.gather(*tasks)
+            results = await asyncio.gather(*tasks)
+            return [result for result in results if result is not None]  # Удаляем None
 
 class WebParser:
     async def parse_data(self, html_content):
-        try:
-            soup = BeautifulSoup(html_content[0], 'lxml')
-            parsed_data = {
-                'title' : soup.find('meta', attrs={'name': 'title'})['content'],
-                'tags' : [tag['content'] for tag in soup.find_all('a', class_='tm-tags-list__link')]
-            }
-            return parsed_data
-        except Exception as e:
-            print(f'Error: {e}')
-            return {}
-
+        data = []
+        for html in html_content:
+            try:
+                soup = BeautifulSoup(html, 'lxml')
+                parsed_data = {
+                    'title': soup.find(class_='tm-title tm-title_h1').text if soup.find(class_='tm-title tm-title_h1') else 'No Title',
+                    'tags': [tag.text for tag in soup.find_all('a', class_='tm-tags-list__link')]
+                }
+                data.append(parsed_data)
+            except Exception as e:
+                print(f'Error: {e}')
+        return data
 class Pipeline:
     def __init__(self, urls):
         self.scraper = WebScraper(urls)
@@ -60,8 +62,26 @@ class Pipeline:
 
         print('Pipeline completed!')
 
+def main_menu():
+    urls = []
+    print('\nДобро пожаловать в Персональный помощник!')
+    print('Введите ссылку на статью Хабра:')
+    urls.append(input())
+    while True:
+        print('Добавить ещё одну ссылку?')
+        print('1. Да')
+        print('2. Нет, хватит')
+        choice = input('Введите номер действия: ')
+        if choice == '1':
+            urls.append(input('Введите ссылку: '))
+        elif choice == '2':
+            return urls
+
+
 if __name__ == "__main__":
-    urls = ['https://habr.com/ru/articles/870642/']
+    # urls = ['https://habr.com/ru/articles/870642/', 'https://habr.com/ru/articles/871426/']
+
+    urls = main_menu()
 
     runner = Pipeline(urls)
 
